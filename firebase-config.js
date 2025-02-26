d472d5f4fa85afd4061bc87cb2d051daedf1ae60
mahiravi777 <paladuguravikumar624@gmail.com>
2025-02-26 07:57:28 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA4LG8DwtEGn99KeG1jDBGt82qs3a5C9Cg",
+  authDomain: "cityride-6f0ba.firebaseapp.com",
+  databaseURL: "https://cityride-6f0ba-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cityride-6f0ba",
+  storageBucket: "cityride-6f0ba.firebasestorage.app",
+  messagingSenderId: "12444627870",
+  appId: "1:12444627870:web:f3b6b1cea05c14654c1c8f",
+  measurementId: "G-6D4XLZDR2E"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const auth = firebase.auth();
