eae991035e555f28d394b65a60865665e5fb4c22
mahiravi777 <paladuguravikumar624@gmail.com>
2025-04-15 12:34:44 +0530

    tracking.js

--- tracking.js
+++ tracking.js
@@ -2,7 +2,7 @@ let map, userMarker, driverMarker;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 0, lng: 0 }, 
+        center: { lat: 0, lng: 0 },
         zoom: 15
     });
 
